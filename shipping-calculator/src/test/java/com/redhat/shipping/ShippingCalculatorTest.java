e0bc0c32eff330fe3dd679fe42bb0bfc86a68d91
Gang Cheng <lillian@Lillians-MacBook-Pro-2.local>
2024-02-01 22:42:01 +0100

    refactor

--- shipping-calculator/src/test/java/com/redhat/shipping/ShippingCalculatorTest.java
+++ shipping-calculator/src/test/java/com/redhat/shipping/ShippingCalculatorTest.java
@@ -8,3 +8,14 @@ public class ShippingCalculatorTest {
 
     // @todo: add tests
 }
+@Test
+public void canCalculateTheCostForARegion() {
+    ShippingCalculator calculator = new ShippingCalculator();
+
+    assertEquals(0, calculator.costForRegion("A Region"));
+}
+@Test
+public void onNARegionTheCostIs100() {
+    assertEquals(100, (new ShippingCalculator()).costForRegion("NA"));
+}
+
